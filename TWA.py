f06bf0643e0e0640ba6e36ccdaf1a4181b21f702
jacqueline ato <paolo_ato@dlsu.edu.ph>
2020-12-01 09:37:52 +0800

    fixed bug in methods

--- TWA.py
+++ TWA.py
@@ -5,57 +5,53 @@ import time
 def insert_hashtag(string, index):
 	return string[:index] + '#' + string[index:]
 
-#Method for inserting an arrow "->" to indicate position in the tape
-def insert_pointer(string, index):
-	return string[:index] + "->" + string[index:]
-
 #Simulates the TWA machine 
 class TWA:
 	def __init__(self, states):
 		self.states = states
 
 	def simulateMachine(self, test):
-		self.currPosition = 1
-
-		# temp = test
-		# # print(f"Temporary String Test: {temp}")
-		# temp = insert_pointer(temp, 1)
-		# print(f"Tape Input: {temp}")
-
 		# Starts at state 1
-		self.currentState = 1
+		self.currPosition = 1
+		self.currState = 1
+		moveCtr = 0
 		
 		try:
 			#while loop runs as long as the accept and reject states are not yet reached
 			inv = ''
-			while self.states[self.currentState].getMove() != 'accept' and self.states[self.currentState].getMove() != 'reject':
+			while self.states[self.currState].getMove() != 'accept' and self.states[self.currState].getMove() != 'reject':
 				time.sleep(1.5)
-				print("==============================================================================")
-				print(f"Currently at State: {str(self.currentState)}")
+				print("==========================================")
+				print(f"Currently at State: {str(self.currState)}")
 
 				char = test[self.currPosition]
 				inv = char
 				print(f"Input: {char}")
-				transition = self.states[self.currentState].getTransition(char)
-				self.currentState = int(transition)
+				transition = self.states[self.currState].getTransition(char)
+				self.currState = int(transition)
 
 				print(f"Moving to State: {str(transition)}")
-				if (self.states[self.currentState].getMove() == 'right' or self.states[self.currentState].getMove() == 'left'):
-					print(f"Action: Moving arrow to the {self.states[self.currentState].getMove()}")
+				if (self.states[self.currState].getMove() == 'right' or self.states[self.currState].getMove() == 'left'):
+					print(f"Action: Moving arrow pointer to the {self.states[self.currState].getMove()}")
 				else:
 					print("Action: Last State Reached")
+					moveCtr += 1
 
-				if self.states[self.currentState].getMove() == 'right':
+				if self.states[self.currState].getMove() == 'right':
 					self.currPosition += 1
-				elif self.states[self.currentState].getMove() == 'left':
+					moveCtr += 1
+				elif self.states[self.currState].getMove() == 'left':
 					self.currPosition -= 1
+					moveCtr += 1
 
-			print(f"Result of the input: {self.states[self.currentState].getMove()}")
-			print("==============================================================================")
+			print("==========================================")
+			print(f"Number of total moves taken: {moveCtr}")
+			print(f"Result of the input: {self.states[self.currState].getMove().upper()}")
+			print("==========================================")
 		except KeyError:
 			print(f"Invalid Character '{inv}' Found (Not in List of Accepted Input Characters)")
 			print("Input is Rejected for this TWA")
-			print("==============================================================================")
+			print("==========================================")
 
 #Object that holds the information of each state from the CSV File
 class State:
@@ -84,12 +80,12 @@ test4 = "5a_3b.csv"
 test5 = "3a_More-b.csv"
 
 #Open CSV Files
-with open(test1) as csv_file:
+with open(test3) as csv_file:
 	csv_reader = csv.reader(csv_file, delimiter=',')
 	state_count = 0
 	states = {}
-	print ("=========TWO WAY ACCEPTER SIMULATION=========")
-	print(f"Loading TWA File: {test1}")
+	print ("======= TWO WAY ACCEPTER SIMULATION ======")
+	print(f"Loading TWA File: {test3}")
 
 	# CSV FORMAT: row[0] = state number, row[1] = action taken, row [2] = char input and transitions
 	for row in csv_reader:
@@ -104,6 +100,7 @@ with open(test1) as csv_file:
 				states[stateNo].setTransition(transition)
 		state_count += 1
 	print(f'Finished loading {state_count} number of states.')
+	print("==========================================")
 
 #Main Program
 # eval = "aabb"
